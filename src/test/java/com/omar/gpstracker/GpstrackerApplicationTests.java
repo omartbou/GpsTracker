7cf012b179f62d95fee0f80392ef6ca1f18f4c74
Omar BOUZIANI <103196882+omartbou@users.noreply.github.com>
2025-03-11 15:14:20 +0000

    Initial commit

---
+++ src/test/java/com/omar/gpstracker/GpstrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omar.gpstracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GpstrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
