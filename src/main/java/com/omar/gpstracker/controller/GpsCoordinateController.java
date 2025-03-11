b9ee4c79b993bf41c9f066afc378c38000b71c01
Omar BOUZIANI <103196882+omartbou@users.noreply.github.com>
2025-03-11 20:56:20 +0000

    feat:add cross origin

--- src/main/java/com/omar/gpstracker/controller/GpsCoordinateController.java
+++ src/main/java/com/omar/gpstracker/controller/GpsCoordinateController.java
@@ -3,6 +3,7 @@ package com.omar.gpstracker.controller;
 import com.omar.gpstracker.model.GpsCoordinate;
 import com.omar.gpstracker.repository.GpsCoordinateRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = "*")
 public class GpsCoordinateController {
 
     @Autowired
